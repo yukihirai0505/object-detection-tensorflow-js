912694e71965ff5f5287866fc7a17c489bdd1c72
Yuki Hirai <yukihirai0505@gmail.com>
2018-12-21 17:22:02 +0900

    Updates

---
+++ precache-manifest.70fc8157b65a6bf80f3eee47f41804f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08623899f1b0c4c59708",
+    "url": "/object-detection-tensorflow-js/static/css/main.41e6faaf.chunk.css"
+  },
+  {
+    "revision": "08623899f1b0c4c59708",
+    "url": "/object-detection-tensorflow-js/static/js/main.08623899.chunk.js"
+  },
+  {
+    "revision": "0e1884a8b4b576c4c1c9",
+    "url": "/object-detection-tensorflow-js/static/js/1.0e1884a8.chunk.js"
+  },
+  {
+    "revision": "ce2d700591aeba203d82",
+    "url": "/object-detection-tensorflow-js/static/js/runtime~main.ce2d7005.js"
+  },
+  {
+    "revision": "fac2045481a82ab5c7602f396bf41798",
+    "url": "/object-detection-tensorflow-js/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4906f4562025caf589c9b9e983a9c46c.js"
+  "/object-detection-tensorflow-js/precache-manifest.70fc8157b65a6bf80f3eee47f41804f2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/object-detection-tensorflow-js/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
